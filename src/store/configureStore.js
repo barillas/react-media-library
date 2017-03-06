f6995ed51e507c8cc3bd866f3834d70ade2d20e0
Oscar Barillas <oscarbarillas93@gmail.com>
2017-03-05 16:03:34 -0800

    Configured store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,52 +1,17 @@
-import {createStore, compose, applyMiddleware} from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
+import rootSaga from '../sagas'; // TODO: Next step
 
-function configureStoreProd(initialState) {
-  const middlewares = [
-    // Add other middleware on this line...
-
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-    thunk,
-  ];
-
-  return createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares)
-    )
-  );
-}
-
-function configureStoreDev(initialState) {
-  const middlewares = [
-    // Add other middleware on this line...
-
-    // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
-    reduxImmutableStateInvariant(),
-
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-    thunk,
-  ];
-
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  const store = createStore(rootReducer, initialState, composeEnhancers(
-    applyMiddleware(...middlewares)
-    )
-  );
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers').default; // eslint-disable-line global-require
-      store.replaceReducer(nextReducer);
-    });
-  }
-
-  return store;
-}
-
-const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
+//  Returns the store instance
+// It can  also take initialState argument when provided
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  return {
+    ...createStore(rootReducer,
+      applyMiddleware(sagaMiddleware)),
+    runSaga: sagaMiddleware.run(rootSaga)
+  };
+};
 
 export default configureStore;
